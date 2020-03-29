b82bbcdaa0cce48d429eb0ed1abd38449d68719f
OverweightCreator <42464672+OverweightCreator@users.noreply.github.com>
2020-03-29 23:38:04 +0300

    Add files via upload

--- Distribution.py
+++ Distribution.py
@@ -9,8 +9,8 @@ dtype = np.dtype(
      ('z', np.double),
      ('time', np.double)
      ])
-
-data=np.fromfile("Electron1.bin",dtype=dtype)
+EVENTS_NUM=10
+data=np.fromfile("ElectronGurevich.bin",dtype=dtype)
 #data=np.load('generic.npy')
 data=data[np.argsort(data,order=('event',"id","time"))]
 data=data[['event','id','z','time']]
@@ -26,7 +26,8 @@ def getTrack(event,trackid):
   left=np.searchsorted(event['id'],trackid,side="left")
   right=np.searchsorted(event['id'],trackid,side="right")
   return event[left:right]
-data=getEventLayer(data,50)
+data=getEventLayer(data,EVENTS_NUM)
+
 gc.collect()
 dt=100
 dz=25
@@ -40,27 +41,41 @@ def processTracks(event,trackIds,time,dt,z,dz):
           track=getTrack(event,trackId)
           
           if track.size==1 and abs(track[0]['time']-time)<dt and abs(track[0]['z']-z)<dz:
-            #return 1
-            result[int(time/dt),int((z-minz)/dz)]=result[int(time/dt),int((z-minz)/dz)]+1
+            return 1
+            #result[int(time/dt),int((z-minz)/dz)]=result[int(time/dt),int((z-minz)/dz)]+1
             
           idTime=np.searchsorted(track['time'],time)
           idZ=np.searchsorted(track['z'],z)
-          if idTime==0 or idTime==track.size or idZ==0 or idZ==track.size or abs(track[idTime]['time']-time)>dt or abs(track[idZ]['z']-z)>dz:
-            #return 0
-            pass
+          if track.size==0 or idTime==0 or idTime==track.size or idZ==0 or idZ==track.size or abs(track[idTime]['time']-time)>dt or abs(track[idZ]['z']-z)>dz:
+            return 0
+            #pass
           else:
-            #return 1
-            result[int(time/dt),int((z-minz)/dz)]=result[int(time/dt),int((z-minz)/dz)]+1
+            return 1
+            #result[int(time/dt),int((z-minz)/dz)]=result[int(time/dt),int((z-minz)/dz)]+1
+          return 0
 
-        
-        
+def foo(event,time,dt,z,dz):
+    print(time,z,event[0]['event'])
+    return processTracks(event,np.unique(event['id']),time,dt,z,dz)
+      
+   
 for time in np.arange(0,maxtime,dt):
   for z in np.arange(minz,maxz,dz):
-    print(time,z)
+  
+    args=[]        
+    
+    for eventId in range(EVENTS_NUM):
+       event=getEvent(data,eventId)
+       args.append([event,time,dt,z,dz])
+
+    with Pool(3) as p:
+        result[int(time/dt),int((z-minz)/dz)]=result[int(time/dt),int((z-minz)/dz)]+sum(p.starmap(foo,args))
+    """
     for eventId in np.unique(data['event']):
       event=getEvent(data,eventId)
       processTracks(event,np.unique(event['id']),time,dt,z,dz)
-
+    """
+result=result/EVENTS_NUM
 np.save('Distribution',result)
         
 
